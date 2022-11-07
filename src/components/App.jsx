328c04220a7362178a46ae217803f98d2685828c
Maks Novakov <maksnovakov78@gmail.com>
2022-11-07 16:15:16 +0200

    components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import React, { Component } from "react";
+// import { fetchImages } from './Services/Api';
+// import { Searchbar } from './Searchbar/Searchbar';
+// import { Loader } from './Loader/Loader';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Button } from './Button/Button';
+// import { animateScroll } from './react-scroll';
+// import { Modal } from './Modal/Modal';
