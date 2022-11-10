c974ee2c3fa8b7be305300c64635962f5e48bb7f
Maks Novakov <maksnovakov78@gmail.com>
2022-11-10 12:16:34 +0200

    3

--- src/components/Services/Api.jsx
+++ src/components/Services/Api.jsx
@@ -10,15 +10,3 @@ export const http = {
 };
 
 
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-// axios.defaults.params = {
-//     key: '30051672-08dafe1edcff0340557e21f5f',
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     per_page: 12,
-// };
-
-// export const fetchImages = async (query,page) => {
-//     const { data } = await axios.get(`?q=${query}&page=${page}`);
-//     return data;
-// }
